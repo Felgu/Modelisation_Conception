121c0eecb4cd984aeb7997970a88173750a42c52
maaddoo <mugisha.madona@gmail.com>
2023-08-01 18:16:02 -0400

    ajouter la suppression du projet

--- TimeLogV01/src/main/java/service/AdminService.java
+++ TimeLogV01/src/main/java/service/AdminService.java
@@ -7,13 +7,14 @@ public class AdminService {
 		
 		System.out.println("-- Menu Admin--");
 		System.out.println("1. Créer un projet");
-		System.out.println("2. Ajouter un employé");
-		System.out.println("3. Assigner un employé au projet");
-		System.out.println("4. Ajouter une discipline au projet");
-		System.out.println("5. Supprimer un employé");
-		System.out.println("6. Changer le NPE d'un employé");
-		System.out.println("7. Modifier le nom d'un utilisateur");
-		System.out.println("8. Modifier le ID"); 
+		System.out.println("2. Supprimer un projet");
+		System.out.println("3. Ajouter un employé");
+		System.out.println("4. Assigner un employé au projet");
+		System.out.println("5. Ajouter une discipline au projet");
+		System.out.println("6. Supprimer un employé");
+		System.out.println("7. Changer le NPE d'un employé");
+		System.out.println("8. Modifier le nom d'un utilisateur");
+		System.out.println("9. Modifier le ID"); 
 	}
 	
 	
