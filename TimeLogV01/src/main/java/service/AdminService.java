bf37a86956fbb5afbe960075f1dd4c438f8e177a
pires lufungula <pires00243@gmail.com>
2023-08-01 18:11:28 -0400

    Liste menu admin

--- TimeLogV01/src/main/java/service/AdminService.java
+++ TimeLogV01/src/main/java/service/AdminService.java
@@ -8,9 +8,12 @@ public class AdminService {
 		System.out.println("-- Menu Admin--");
 		System.out.println("1. Créer un projet");
 		System.out.println("2. Ajouter un employé");
-		System.out.println("3. Assigner un employe au projet");
-		System.out.println("4. Créer un projet");
-		System.out.println("5. Créer un projet");
+		System.out.println("3. Assigner un employé au projet");
+		System.out.println("4. Ajouter une discipline au projet");
+		System.out.println("5. Supprimer un employé");
+		System.out.println("6. Changer le NPE d'un employé");
+		System.out.println("7. Modifier le nom d'un utilisateur");
+		System.out.println("8. Modifier le ID"); 
 	}
 	
 	
